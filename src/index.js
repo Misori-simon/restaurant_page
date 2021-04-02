13aed4b3d36201eaf605931919fd09d8850aea51
Misori-simon <misorisimon@gmail.com>
2021-04-02 12:40:16 +0100

    Add a pageLoad module for loading the header - Generated header of index page from a pageLoad module - import this pageLoad module into index js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
+import pageLoad from './page-load';
+
+pageLoad();
 const show = () => 'I am fine';
 console.log(show());
---
+++ src/page-load.js
@@ -0,0 +1,55 @@
+const pageLoad = () => {
+  function navlinkGenerator(parent, arr, className) {
+    for (let i = 0; i < arr.length; i += 1) {
+      const li = document.createElement('li');
+      li.classList.add(className);
+
+      const a = document.createElement('a');
+      a.setAttribute('href', '#');
+      a.innerHTML = arr[i];
+
+      li.appendChild(a);
+      parent.appendChild(li);
+    }
+  }
+
+  const contentDiv = document.getElementById('content');
+
+  const header = document.createElement('header');
+  header.classList.add('header');
+  // for logo and tagline
+  const logoWrap = document.createElement('a');
+  logoWrap.setAttribute('href', 'index.html');
+  logoWrap.setAttribute('title', 'home');
+  logoWrap.classList.add('logo');
+
+  const logoImage = document.createElement('img');
+  logoImage.setAttribute('src', './logo.png');
+  logoImage.setAttribute('alt', 'fembe eats');
+  logoWrap.appendChild(logoImage);
+
+  const tagline = document.createElement('img');
+  tagline.classList.add('tagline');
+  tagline.innerHTML = 'Eat fresh, stay healthy';
+  logoWrap.appendChild(tagline);
+
+  header.appendChild(logoWrap);
+
+  // for navigation
+  const nav = document.createElement('nav');
+  nav.classList.add('nav-wrap');
+
+  const ul = document.createElement('ul');
+  ul.classList.add('nav');
+
+  navlinkGenerator(ul, ['Home', 'Order', 'About Us', 'Contact US'], 'nav-items');
+
+
+  nav.appendChild(ul);
+
+  header.appendChild(nav);
+
+  contentDiv.appendChild(header);
+};
+
+export default pageLoad;
